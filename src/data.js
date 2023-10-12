5fe33b3f043c73862f0d82421d4f08575d1baf4e
Pratyush Ujjwal <109185262+pratyushujjwal@users.noreply.github.com>
2023-10-13 00:49:25 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const projects = [{
+        title: "Stopwatch",
+        subtitle: "FrontEnd Web",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://media.istockphoto.com/id/1171581218/vector/set-of-timer-icons-on-white-background-vector-illustration-in-flat-design.jpg?s=612x612&w=0&k=20&c=3TyJga9Vxbi7FuKTZqx8fDour577gfktwWMBXcScrlI=",
+        link: "https://github.com/pratyushujjwal",
+    },
+    {
+        title: "Dashboard",
+        subtitle: "FrontEnd Web",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://t4.ftcdn.net/jpg/02/70/95/21/360_F_270952103_2zSDVMWHM7KFOXmO0Dko0pYOE9aCs07k.jpg",
+        link: "https://github.com/pratyushujjwal",
+    },
+    {
+        title: "Navigation",
+        subtitle: "FrontEnd Web",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://t4.ftcdn.net/jpg/01/10/45/19/360_F_110451937_gia9fnBBtkDAAfb3rTvmIAbnngjf7ACe.jpg",
+        link: "https://github.com/pratyushujjwal",
+    },
+    {
+        title: "Random User",
+        subtitle: "JavaScript",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://t4.ftcdn.net/jpg/06/39/60/09/240_F_639600962_xJFyRPbSYVvPbyO63onS7O0Uu9jyvhPX.jpg",
+        link: "https://github.com/pratyushujjwal",
+    },
+];
